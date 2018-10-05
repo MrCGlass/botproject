942ad0d8cd97e6719f70426bfe4c0cb3206b2339
MrCGlass <cglass488gmail.com>
2018-10-05 16:16:20 -0400

    'first'

---
+++ appBot.py
@@ -0,0 +1,28 @@
+import wx,requests,json
+import virtualbox
+
+
+class mainscreen(wx.Panel):
+
+    def __init__(self,*args,**kwargs):
+        super(mainscreen,self).__init__(*args,**kwargs)
+
+
+class mainApp(wx.Frame):
+
+
+    def __init__(self,*args,**kwargs):
+        super(mainApp,self).__init__(*args,**kwargs)
+        self.screen=mainscreen(self)
+    
+
+
+def main():
+    app = wx.App()
+    window = mainApp(None,wx.ID_ANY,size=(900,600))
+    window.Show(True)
+    app.MainLoop()
+    
+
+if __name__ == '__main__':
+    main()
